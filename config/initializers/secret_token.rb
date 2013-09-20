d7d4f40276d40b1d3e243cef6873597a32d06631
cindyzamora <cindy@bluekite.com>
2013-09-20 02:25:12 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpeedRead::Application.config.secret_token = '7c6ff6835bf2bb3a782f0dc2b9bb007ab6f2b30186251905d505320b83d4a1703582ed7a4cf77b7ac72587e0e7d134f88f7a1f396336c2dcec652be82e5e5496'
